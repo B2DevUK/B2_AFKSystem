d337838d6626a21897e483ec2008a9a930fc90b5
B2DevUK <115435623+B2DevUK@users.noreply.github.com>
2024-07-07 16:37:51 +0100

    Update fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'B2DevUK'
+description 'A simple AFK System Script'
+version '1.0.0'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+shared_scripts {
+    'shared/config.lua'
+}
