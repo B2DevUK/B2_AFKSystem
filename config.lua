d5a5c618a6c51ee2749867abb5acd0c9fb23a562
B2DevUK <115435623+B2DevUK@users.noreply.github.com>
2024-07-19 05:59:20 +0100

    v1.0.1

---
+++ config.lua
@@ -0,0 +1,18 @@
+Config = {}
+
+-- Enable/disable AFK system
+Config.AFKSystemEnabled = true
+
+-- Idle time in seconds before a player is considered AFK
+Config.IdleTimeThreshold = 10
+
+-- Time in seconds before an AFK player is kicked
+Config.KickTime = 30
+
+-- Enable/disable invincibility for AFK players
+Config.InvincibilityEnabled = true
+
+-- Enable/disable AFK icon display
+Config.AFKIconEnabled = true
+
+Config.IgnoredGroups = {"mod", "admin", "god"} -- groups to ignore for AFK check
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,16 +3,16 @@ game 'gta5'
 
 author 'B2DevUK'
 description 'A simple AFK System Script'
-version '1.0.0'
+version '1.0.1'
 
 client_scripts {
-    'client/client.lua'
+    'client.lua'
 }
 
 server_scripts {
-    'server/server.lua'
+    'server.lua'
 }
 
 shared_scripts {
-    'shared/config.lua'
+    'config.lua'
 }
